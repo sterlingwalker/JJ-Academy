537dada7a523545c3be94ce5cdbcfc7a7d81248c
sterlingwalker <68869426+sterlingwalker@users.noreply.github.com>
2022-02-14 21:15:17 -0500

    Adds comments to home screen

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import * as React from 'react';
 import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
@@ -10,9 +10,25 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Comments from "./Comments";
+
 
 
 export default function Home() {
+    const comments = [
+        {name: 'Thomas Holloway', comment: 'This was such an epic match!'}, 
+        {name: 'Max Parker', comment: 'I really liked his technique, I need to learn that move myself'}, 
+        {name: 'John Doe', comment: 'Does anyone know the name of the move he made at 4:32?'}]
+
+    const [cmt, setCmt] = React.useState(comments)
+    const [currentComment, setCurrentComment] = React.useState('')
+
+    const handleComment = (event) => {
+        // Will change the name when we get other stuff hooked up
+        const newComment = {name: 'Sterling Walker', comment: currentComment}
+
+        setCmt([...cmt, newComment])
+    }
 
     return (
         <Grid container spacing={2}>
@@ -40,13 +56,15 @@ export default function Home() {
             <Grid item xs={8}>
                 <Card variant="outlined">
                     <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        <Typography sx={{ fontSize: 24 }}  gutterBottom>
                             Most Recently uploaded video
                         </Typography>
-                        <iframe width="760" height="480" src="https://www.youtube.com/embed/_L-Ni7bFAHg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="760" height="480" src="https://www.youtube.com/embed/_L-Ni7bFAHg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <Comments comments={cmt} />
+                        <TextField label="Enter a comment" variant="outlined" value={currentComment} onChange={event => setCurrentComment(event.target.value)} sx={{width: '95%', marginLeft: '20px'}} />
                     </CardContent>
                     <CardActions>
-                        <Button size="small">Learn More</Button>
+                        <Button size="small" variant="contained" onClick={handleComment}>Add Comment</Button>
                     </CardActions>
                 </Card>
             </Grid>
