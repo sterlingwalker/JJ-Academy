fdd056df827d8feeb7340d9db4d7cbd57c5668e4
TommyH92 <71985203+TommyH92@users.noreply.github.com>
2022-03-02 18:42:12 -0500

    index issue
    i think it has an issue with the installation with the NPM maybe you know

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,3 +27,47 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const express = require("express");
+const mysql = require("mysql");
+const ejs = require("ejs");
+
+// Create express app
+const app = express();
+
+// Create a database connection configuration
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Tommy1252",
+  database: "destinygearinfo", // comment out if running example 1
+});
+
+// Establish connection with the DB
+db.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`Successful connected to the DB....`);
+  }
+});
+
+// Initialize Body Parser Middleware to parse data sent by users in the request object
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse HTML form data
+
+// Initialize ejs Middleware
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+// routes
+app.get("/", (req, res) => {
+res.render("index.ejs");
+});
+
+let sql;
+let data;
+let query;
+
+
