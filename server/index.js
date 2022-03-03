5c1a0f64d47750e95a74ca5947dcec7ca9390290
TommyH92 <71985203+TommyH92@users.noreply.github.com>
2022-03-02 19:23:26 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const mysql = require("mysql");
+const ejs = require("ejs");
 
 const PORT = process.env.PORT || 3001;
 
@@ -21,3 +23,40 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
 	  console.log(`Server listening on ${PORT}`);
 });
+
+
+// Create a database connection configuration
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Tommy1262",
+  database: "jj-AcademyDB", 
+});
+
+// Establish connection with the DB
+db.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`Successful connected to the DB....`);
+  }
+});
+
+// Initialize Body Parser Middleware to parse data sent by users in the request object
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse HTML form data
+
+// Initialize ejs Middleware
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+// routes
+app.get("/", (req, res) => {
+res.render("index.ejs");
+});
+
+let sql;
+let data;
+let query;
+
+
