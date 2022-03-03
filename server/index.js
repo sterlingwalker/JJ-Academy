4b8eca5bbf6d6309fcc5bc8186b95d76ae30d3e8
TommyH92 <71985203+TommyH92@users.noreply.github.com>
2022-03-02 20:30:31 -0500

    testestest

--- server/index.js
+++ server/index.js
@@ -42,9 +42,7 @@ db.connect((err) => {
   }
 });
 
-// Initialize Body Parser Middleware to parse data sent by users in the request object
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // to parse HTML form data
+
 
 // Initialize ejs Middleware
 app.set("view engine", "ejs");
@@ -54,6 +52,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
 res.render("index.ejs");
 });
+
 
 let sql;
 let data;
